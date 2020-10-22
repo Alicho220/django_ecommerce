16bb8941dc94ae3c0f23fd157415228c4e99414a
Alicho220 <jimmyoalicho@gmail.com>
2020-10-22 21:38:28 +0100

    until payment

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -16,7 +16,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name= models.CharField(max_length=200)
-    price= models.FloatField(null=True, blank=True)
+    price= models.DecimalField(max_digits=7, decimal_places=2)
     image= models.ImageField(null=True, blank=True)
     digital = models.BooleanField(default=False, null= True, blank= False)
 
@@ -43,6 +43,15 @@ class Order(models.Model):
         return str(self.transaction_id)
 
     @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
+
+    @property
     def get_cart_total(self):
         orderitems= self.orderitem_set.all()
         total = sum([item.get_total for item in orderitems])
@@ -53,7 +62,6 @@ class Order(models.Model):
         orderitems= self.orderitem_set.all()
         total = sum([item.quantity for item in orderitems])
         return total
-
 
 
 class OrderItem(models.Model):
